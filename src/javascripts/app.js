b0670d51b14207e6cd81076d915d665da859ddcb
cz160 <1604506998@qq.com>
2018-10-15 17:38:00 +0800

    'version-1'

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,3 +1,8 @@
 
+import home_controller from './controllers/home_controller';
+import home_templat from './views/home.html';
+//渲染路由
+$('.warp').html(home_templat);
+home_controller.render();
 
 
--- src/javascripts/router/route.js
+++ src/javascripts/router/route.js
@@ -1,9 +1,6 @@
 
-
 const routes = {
     '#/job': job_controller,
-    '#/search': search_controller,
-    '#/profile': profile_controller
 }
 
 export { routes } ;
