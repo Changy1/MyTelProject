2f624158c8269c6c1e0f58000ae48bda83bb2951
cz160 <1604506998@qq.com>
2018-10-15 20:10:52 +0800

    'routerfinish'

--- src/javascripts/controllers/register_controller.js
+++
@@ -1,8 +0,0 @@
-import register_template from  '../views/register.html';
-
-const render = ()=>{
-    $('.home-container').html(register_template);
-}
-export default{
-    render
-}
--- src/javascripts/router/route.js
+++ src/javascripts/router/route.js
@@ -1,11 +1,9 @@
 //路由表
 import home_job_controller from '../controllers/home_job_controller';
 import job_controller from '../controllers/job.controller';
-import register_controller from '../controllers/register_controller';
 const routes = {
     '#/home_job': home_job_controller,
     '#/job':job_controller,
-    '#/register':register_controller
 }
 
 export { routes } ;
