fd811749bdf08c1cbc6dc4469036bede20bf8619
Your Name <you@example.com>
2018-10-16 19:04:20 +0800

    '10.16-one'

--- src/javascripts/controllers/home_job_controller.js
+++ src/javascripts/controllers/home_job_controller.js
@@ -1,7 +1,13 @@
 import home_job_template from '../views/home-job.html';
+import home_job_list from '../views/home-job-list.html';
+import job_list from '../models/home_job_model';
 
 const render = ()=>{
     $('.home-container').html(home_job_template);
+
+    let _p = 1;
+    getJoblist(_p);
+
     var mySwiper = new Swiper('.swiper-container', {
         autoplay: {
             delay: 2500,//1秒切换一次
@@ -13,6 +19,24 @@ const render = ()=>{
         },
     })
 }
+
+const getJoblist = async (_p)=>{
+    let date = await job_list.job_list(_p);
+    let _job_data = JSON.parse(date);
+    let _job_list = _job_data.msg;
+
+    let _template = Handlebars.compile(home_job_list);
+    let _html = _template({_job_list});
+    //这里当他请求完成以后会开启一个两秒的定时器。
+    setTimeout(function(){
+        $('.list').html('');
+    },2000)
+    setTimeout(function(){
+        $('.list').append(_html);
+    },2000)
+}
+
+
 export default{
     render
 }
