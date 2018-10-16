957ca245cc309a26b9255b15c22e2705c756a184
cz160 <1604506998@qq.com>
2018-10-16 09:16:00 +0800

    'foru'

--- src/javascripts/controllers/job.controller.js
+++ src/javascripts/controllers/job.controller.js
@@ -2,14 +2,13 @@ import job_template from  '../views/job.html';
 import job_content_template from '../views/job-content.html';
 import job_list from '../models/home_job_model';
 const render = ()=>{
-    //初始加载一下框架
-    let _template = Handlebars.compile(job_template);
-    let _html = _template();
-    $('.home-container').html(_html);
+    //初始将job页面渲染到相对应的页面
+    $('.home-container').html(job_template);
     //当前的加载职业页数
     let _p= 1;
     //初始加载第一页
     getJobList(_p);
+    // loadMore(_p);
    
 }
 //等待获取某一页数据然后渲染
@@ -19,12 +18,19 @@ const getJobList=async(_p)=>{
     let  _job_data= JSON.parse(data);
     //多个职位的数组信息
     let _job_list= _job_data.msg;
-    console.log(_job_list);
+    // console.log(_job_list);
     let _template = Handlebars.compile(job_content_template);
     // 将handlebar模板编译成html格式的字符串
     let _html = _template({ _job_list});
     $('.list-box').append(_html);
 }
+//点击加载更多
+// const loadMore=(_p)=>{
+//     $('.content .more').on('tap',function(){
+//         _p++;
+//         getJobList(_p);
+//     })
+// }
 export default{
     render
 }
